e02f846d2aeaf7382703b004287c2f3ed56434d2
DerRomtester <dieb.stefan.96@gmail.com>
2015-03-22 19:22:02 +0100

    a

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 make clean && make mrproper
 export ARCH=arm
 export SUBARCH=arm
-export CROSS_COMPILE=/home/jgcaap/android/prebuilts/gcc/linux-x86/arm/arm-eabi-4.9/bin/arm-eabi-
+export CROSS_COMPILE=/home/jgcaap/android/prebuilts/gcc/linux-x86/arm/arm-eabi-5.0/bin/arm-eabi-
 export KBUILD_BUILD_USER=jgcaap
 export KBUILD_BUILD_HOST=kernel
 make cyanogenmod_bacon_defconfig
