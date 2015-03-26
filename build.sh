055cdb18502ecd1d3674d16969a6f4462c710155
ZaneZam <cyxman@yahoo.com>
2015-03-26 14:56:22 +0100

    toolchain: multiple fixes and changes for gcc 5.0.0
    Conflicts:
    	kernel/Makefile
    	mm/rmap.c

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
-make clean
+make clean && make mrproper
 export ARCH=arm
 export SUBARCH=arm
-export CROSS_COMPILE=/home/jgcaap/android/prebuilts/gcc/linux-x86/arm/arm-eabi-5.0/bin/arm-eabi-
+export CROSS_COMPILE=/home/jgcaap/android/prebuilts/gcc/linux-x86/arm/arm-eabi-4.9/bin/arm-eabi-
 export KBUILD_BUILD_USER=jgcaap
 export KBUILD_BUILD_HOST=kernel
 make cyanogenmod_bacon_defconfig
 make -j5
+cd ..
 cd ramdisk
 ./cleanup.sh
 ./zip.sh
