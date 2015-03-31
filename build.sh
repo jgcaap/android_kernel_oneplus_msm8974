b8c4f939bfbe32f0d1a86828fdb688ada8eae30a
jgcaap <jgcaap@gmail.com>
2015-03-31 16:15:50 +0100

    Revert "toolchain: multiple fixes and changes for gcc 5.0.0"
    This reverts commit 055cdb18502ecd1d3674d16969a6f4462c710155.

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-make clean && make mrproper
+make clean
 export ARCH=arm
 export SUBARCH=arm
 export CROSS_COMPILE=/home/jgcaap/android/prebuilts/gcc/linux-x86/arm/arm-eabi-5.0/bin/arm-eabi-
@@ -6,7 +6,6 @@ export KBUILD_BUILD_USER=jgcaap
 export KBUILD_BUILD_HOST=kernel
 make cyanogenmod_bacon_defconfig
 make -j5
-cd ..
 cd ramdisk
 ./cleanup.sh
 ./zip.sh
