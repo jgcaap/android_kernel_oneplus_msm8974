bd022fc6b6361089bab1ba281f00c474dd9de273
jgcaap <jgcaap@gmail.com>
2015-04-01 02:41:01 +0100

    corrections

--- build.sh
+++ build.sh
@@ -2,8 +2,6 @@ make clean
 export ARCH=arm
 export SUBARCH=arm
 export CROSS_COMPILE=/home/jgcaap/android/prebuilts/gcc/linux-x86/arm/arm-eabi-5.0/bin/arm-eabi-
-export KBUILD_BUILD_USER=jgcaap
-export KBUILD_BUILD_HOST=kernel
 make cyanogenmod_bacon_defconfig
 make -j5
 cd ramdisk
