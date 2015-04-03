0dbf5efb22e89b3382a2e8707c6de71c89827710
jgcaap <jgcaap@gmail.com>
2015-04-03 10:38:27 +0100

    test

---
+++ kernel.sh
@@ -0,0 +1,131 @@
+#!/bin/bash
+
+# Bash Color
+green='\033[01;32m'
+red='\033[01;31m'
+blink_red='\033[05;31m'
+restore='\033[0m'
+
+clear
+
+# Resources
+THREAD="-j$(grep -c ^processor /proc/cpuinfo)"
+KERNEL="zImage"
+DTBIMAGE="dtb"
+DEFCONFIG="cyanogenmod_bacon_defconfig"
+
+# Kernel Details
+BASE_AK_VER="0.1"
+VER=".CM12.1"
+AK_VER="$BASE_AK_VER$VER"
+
+# Vars
+export CROSS_COMPILE=${HOME}/android/prebuilts/gcc/linux-x86/arm/Stock/arm-eabi-4.8/bin/arm-eabi-
+export ARCH=arm
+export SUBARCH=arm
+export KBUILD_BUILD_USER=jgcaap
+export KBUILD_BUILD_HOST=kernel
+export CCACHE=ccache
+
+# Paths
+KERNEL_DIR=`pwd`
+REPACK_DIR="${HOME}/new/anykernel"
+PATCH_DIR="${HOME}/new/anykernel"
+MODULES_DIR="${HOME}/new/modules"
+ZIP_MOVE="${HOME}/new/out"
+ZIMAGE_DIR="${HOME}/msm8974/arch/arm/boot"
+
+# Functions
+function clean_all {
+		rm -rf $MODULES_DIR/*
+		cd $REPACK_DIR
+		rm -rf $KERNEL
+		rm -rf $DTBIMAGE
+		cd $KERNEL_DIR
+		echo
+		make clean && make mrproper
+}
+
+function make_kernel {
+		echo
+		make $DEFCONFIG
+		make $THREAD
+		cp -vr $ZIMAGE_DIR/$KERNEL $REPACK_DIR
+}
+
+function make_modules {
+		rm `echo $MODULES_DIR"/*"`
+		find $KERNEL_DIR -name '*.ko' -exec cp -v {} $MODULES_DIR \;
+}
+
+function make_dtb {
+		$REPACK_DIR/tools/dtbToolCM -2 -o $REPACK_DIR/$DTBIMAGE -s 2048 -p scripts/dtc/ arch/arm/boot/
+}
+
+function make_zip {
+		cd $REPACK_DIR
+		zip -r9 newKernel-CM12-"$VARIANT".zip *
+		mv newKernel-CM12-"$VARIANT".zip $ZIP_MOVE
+		cd $KERNEL_DIR
+}
+
+
+DATE_START=$(date +"%s")
+
+echo -e "${green}"
+echo "Render Kernel Creation Script:"
+echo -e "${restore}"
+
+while read -p "Do you want to clean stuffs (y/n)? " cchoice
+do
+case "$cchoice" in
+	y|Y )
+		clean_all
+		echo
+		echo "All Cleaned now."
+		break
+		;;
+	n|N )
+		break
+		;;
+	* )
+		echo
+		echo "Invalid try again!"
+		echo
+		;;
+esac
+done
+
+echo
+
+while read -p "Do you want to build kernel (y/n)? " dchoice
+do
+case "$dchoice" in
+	y|Y)
+		make_kernel
+		make_dtb
+		make_modules
+		make_zip
+		break
+		;;
+	n|N )
+		break
+		;;
+	* )
+		echo
+		echo "Invalid try again!"
+		echo
+		;;
+esac
+done
+
+echo -e "${green}"
+echo "-------------------"
+echo "Build Completed in:"
+echo "-------------------"
+echo -e "${restore}"
+
+DATE_END=$(date +"%s")
+DIFF=$(($DATE_END - $DATE_START))
+echo "Time: $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds."
+echo
