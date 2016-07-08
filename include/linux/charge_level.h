e7ae952bc117ea7d28e2daaef0979f2b34c0b2ec
Jorge Ayres Pereira <jgcaap@gmail.com>
2016-07-08 21:40:26 +0200

    Change default charge levels

--- include/linux/charge_level.h
+++ include/linux/charge_level.h
@@ -21,10 +21,10 @@ extern int charge_level;
 extern int charge_info_level_cur;
 extern int charge_info_level_req;
 
-#define AC_CHARGE_LEVEL_DEFAULT 0	// 0 = stock charging logic will apply
+#define AC_CHARGE_LEVEL_DEFAULT 1500	// 0 = stock charging logic will apply
 #define AC_CHARGE_LEVEL_MIN 0
 #define AC_CHARGE_LEVEL_MAX 2100
 
-#define USB_CHARGE_LEVEL_DEFAULT 0	// 0 = stock charging logic will apply
+#define USB_CHARGE_LEVEL_DEFAULT 500	// 0 = stock charging logic will apply
 #define USB_CHARGE_LEVEL_MIN 0
 #define USB_CHARGE_LEVEL_MAX 900
